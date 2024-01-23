594a1e07ec83f5535acfa6c856010340ed2a8d16
Rollly <31448610+rollly00@users.noreply.github.com>
2024-01-24 02:11:25 +0600

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ updater = Updater(token=token,
                   use_context=True)
 dispatcher = updater.dispatcher
 
-wrds = ['tiktok.com', 'instagram.com', 'русню', 'русня', 'резать','ножи','точим','точу','нож','поздняков','Поздняков.']
+wrds = ['tiktok.com', 'instagram.com', 'русню', 'русня', 'резать','ножи','точим','точу','нож','https://t.me/+k_Z9AGYLs7g5ZGUy']
 
 
 def start(update, context):
@@ -16,22 +16,14 @@ def start(update, context):
 
 
 def delete_links(update, context):
-    message = update.message
-    if message and message.text:
-        # Проверка по запрещенным словам
-        for word in wrds:
-            if word in message.text:
-                time.sleep(5)
-                context.bot.delete_message(chat_id=update.effective_chat.id,
-                                           message_id=update.message.message_id)
-                return  # Выход из функции после удаления сообщения
-
-        # Проверка по каналу отправителя
-        if message.forward_from_chat and message.forward_from_chat.username == 'Поздняков 3.0':
-            time.sleep(5)
-            context.bot.delete_message(chat_id=update.effective_chat.id,
-                                       message_id=update.message.message_id)
-
+     message = update.message
+     if message and message.text:
+         for word in wrds:
+             if word in message.text:
+                 time.sleep(5)
+                 context.bot.delete_message(chat_id=update.effective_chat.id,
+                                            message_id=update.message.message_id)
+                 break  # выход из цикла после удаления первого найденного слова
 
 start_handler = CommandHandler('start', start)
 delete_handler = MessageHandler(Filters.text & (~Filters.command),
